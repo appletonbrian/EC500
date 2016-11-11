7ce2ee0f64af5402ff27f4ad30b89fe1d938c386
Brian Appleton <appleton@bu.edu>
2016-11-11 00:14:05 -0500

    first draft of plots complete

--- HW7/problem_1.py
+++ HW7/problem_1.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Nov 10 13:15:55 2016
-
-@author: brian
+@author: Brian Appleton
 """
 
 import matplotlib.pyplot as plt
@@ -28,7 +27,6 @@ c2 = 133e-9 #lambda_p
 c3 = 12000e-9 #lambda_gamma
 
 #Define array for wavelength. 0.1nm resolution.
-#wavelength = np.linspace(390e-9,810e-9,4200)
 wavelength = np.linspace(290e-9, 1010e-9, 7201)
 
 #Calculate the complex relative dielectric constant using the given eqn. (2)
@@ -38,7 +36,6 @@ resonant_e_r = -2*e_water
 
 #Find the index of the element in e_r whose value matches the resonant e_r
 resonant_index = np.abs(np.real(e_r)-resonant_e_r).argmin()
-
 resonant_wavelength = wavelength[resonant_index]
 print(resonant_wavelength*1e9, "nm found at Re{e_r} = ", np.real(e_r[resonant_index]))
 
@@ -72,7 +69,6 @@ er_im = np.imag(e_r)
 
 #Calculate the scattering cross-section using eqn. (17)
 sigma_sca = (8*pi/3)*((2*pi/wavelength)**4)*(R**6)*(((er_re-e_water)**2+er_im**2)/((er_re+2*e_water)**2+er_im**2))**2
-denom = ((er_re+2*e_water)**2+er_im**2)
 
 #Calculate the absorption cross-section using eqn. (19)
 sigma_abs = (4*pi*2*pi/wavelength*R**3)*((er_im*e_water)/((er_re+2*e_water)**2+er_im**2))
@@ -126,6 +122,7 @@ fig.set_size_inches(9,9)
 plt.savefig('prob1_partb.png', dpi=200)
 
 #%% Problem 2
+#Plot the scattering and absorption cross-sections from 300-1000nm on a semilog plot
 
 plt.clf()
 plt.cla()
@@ -152,3 +149,50 @@ plt.xlabel("Wavelength, $nm$", fontsize=14)
 fig = plt.gcf()
 fig.set_size_inches(7,7)
 plt.savefig('prob2.png', dpi=200)
+
+#%%Problem 4
+#Plot the scattering and absorption cross-sections as a function of R^3 at the resonant wavelength
+
+plt.clf()
+plt.cla()
+plt.close()
+
+#Create an array for the cubed radius
+R_cubed = np.linspace(1e-9**3, 10e-9**3, 1001)
+
+#Grab the relative dielectric constants at the resonant wavelength
+er_re_res = er_re[resonant_index]
+er_im_res = er_im[resonant_index]
+wavelength = resonant_wavelength
+
+#Calculate the scattering cross-section using eqn. (17)
+sigma_sca = (8*pi/3)*((2*pi/wavelength)**4)*(R_cubed**2)*(((er_re_res-e_water)**2+er_im_res**2)/((er_re_res+2*e_water)**2+er_im_res**2))**2
+
+#Calculate the absorption cross-section using eqn. (19)
+sigma_abs = (4*pi*2*pi/wavelength*R_cubed**1)*((er_im_res*e_water)/((er_re_res+2*e_water)**2+er_im_res**2))
+
+#Where are the effective cross-sections equal?
+eq_index = np.abs(sigma_sca-sigma_abs).argmin()
+r_cubed_eq = R_cubed[eq_index]
+print("The scattering and absorption cross sections are equal when the particle diameter is ", r_cubed_eq**(1/3)*2e9, "nm.")
+print(r_cubed_eq)
+
+#Plot the scattering cross section
+ax1 = plt.subplot(111)
+plt.plot(R_cubed*1e27, sigma_sca*1e18, label = "Scattering")
+plt.plot(R_cubed*1e27, sigma_abs*1e18, label = "Absorption", linestyle='--')
+plt.plot([5**3, 5**3], [0, 500], linestyle = ':', color ='black', linewidth=1.5)
+plt.plot([r_cubed_eq*1e27, r_cubed_eq*1e27], [0, 500], linestyle = ':', color ='black', linewidth=1.5)
+plt.annotate(s="$d=10nm$", xy=[5**3+15, 153])
+plt.annotate(s="$d=18nm$", xy=[r_cubed_eq*1e27+12, 153])
+plt.suptitle("Scattering and absorption cross-sections for $10nm$ Au NP", fontsize=12)
+plt.ylabel("Effective cross-section, $nm^{2}$", fontsize=12)
+plt.xlabel("$R^{3}, nm^{3}$", fontsize=12)
+ax1.set_xlim(xmin=0, xmax=1000)
+plt.legend(fontsize = 10, loc = 'upper left') 
+ax1.set_ylim(ymin=0, ymax=400)
+plt.grid(True)
+
+fig = plt.gcf()
+fig.set_size_inches(5,5)
+plt.savefig('prob4.png', dpi=200)
